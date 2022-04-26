b7f1109be2f49f413d80720e0895521f715753d9
Your Name <you@example.com>
2022-04-26 04:47:11 +0000

    updated python_git.py

--- python_git.py
+++ python_git.py
@@ -6,11 +6,16 @@ import subprocess
 cwd=os.getcwd()
 repo_name="python-git-demo"
 repo_url="https://github.com/sahilsuri008/"+repo_name
-path=cwd+repo_name
+path=cwd+"/"+repo_name
 
-isFile = os.path.isfile(path) 
+isFile = os.path.isdir(path) 
 
 if isFile:
-	print (path)
+	print ("Pulling latest files from BitBucket")
+	os.chdir(repo_name)
+	subprocess.call(["git", "pull"])
+	os.chdir(cwd)
 else:
+	print(path)
+	print("Repo does not exist. Cloning.")
 	subprocess.call(["git", "clone", repo_url ])
